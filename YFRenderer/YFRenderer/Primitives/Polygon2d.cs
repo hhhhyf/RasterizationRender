2e761365931822dc8fc425fa8d2868c66a57829e
hhhhyf <584026380@qq.com>
2017-04-18 11:10:41 +0800

    scanline

---
+++ YFRenderer/YFRenderer/Primitives/Polygon2d.cs
@@ -0,0 +1,174 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using YFRenderer.Core;
+
+namespace YFRenderer.Primitives
+{
+    class Edge
+    {
+       public int ymax; //边上端的的纵坐标
+       public float x;  //下端点的X坐标
+       public float dx; //斜率的倒数
+    };
+
+    class Polygon2d
+    {
+        public List<Vector2d> Vertices = new List<Vector2d>();
+        List<List<Edge>> EdgeTable = new List<List<Edge>>();
+
+        int ymin = 0;
+        int ymax = 0;
+
+        public void sacnlinefill()
+        {
+            if (Vertices.Count == 0)
+                return;
+
+            ymin = Vertices[0].y;
+            ymax = Vertices[0].y;
+            for (int i = 0; i < Vertices.Count; i++)
+            {
+                if (Vertices[i].y > ymax)
+                {
+                    ymax = Vertices[i].y;
+                }
+
+                if (Vertices[i].y < ymin)
+                {
+                    ymin = Vertices[i].y;
+                }
+            }
+             
+
+            for (int i = 0; i < ymax - ymin + 1; i++)
+            {
+                List<Edge> list = new List<Edge>();
+                EdgeTable.Add(list);
+            }
+
+            InitScanLineNewEdgeTable();
+            HorizonEdgeFill();
+            ProcessScanLineFill();
+        }
+
+        void InitScanLineNewEdgeTable()
+        {
+
+            for (int i = 0; i < Vertices.Count; i++)
+            {
+                Vector2d ps = Vertices[i];
+                Vector2d pe = Vertices[(i + 1) % Vertices.Count];
+                Vector2d pss = Vertices[(i - 1 + Vertices.Count) % Vertices.Count];
+                Vector2d pee = Vertices[(i + 2) % Vertices.Count];
+                Edge e = new Edge();
+                //前一个点和当前点不水平
+                if (pe.y != ps.y)
+                {
+                    e.dx = (float)(pe.x - ps.x) / (float)(pe.y - ps.y);
+                    //后面一个点比当前点高
+                    if (pe.y > ps.y)
+                    {
+                        e.x = ps.x;
+                        //连续单调递减的三个点，中间点要分开，不然水平扫描的时候就奇数个交点了。
+                        if (pee.y >= pe.y)
+                            e.ymax = pe.y - 1;
+                        else
+                            e.ymax = pe.y;
+
+                        EdgeTable[ps.y - ymin].Add(e);
+                    }
+                    else
+                    {
+                        e.x = pe.x;
+                        if (pss.y >= ps.y)
+                            e.ymax = ps.y - 1;
+                        else
+                            e.ymax = ps.y;
+                        EdgeTable[pe.y - ymin] .Add(e);
+                    }
+                }
+            }
+        }
+
+        void InsertNetListToAet (List<Edge> edgeList, ref List<Edge> AET )
+        {
+            for(int i = 0; i < edgeList.Count; i++)
+            {     
+                AET.Add(edgeList[i]);            
+            }
+        }
+
+        void FillAetScanLine(List<Edge> AET, int y)
+        {
+            if (AET.Count <= 1) return;
+            for (int i =0; i < AET.Count; i++)
+            {
+                int x1 = (int)AET[i].x;
+                i++;
+                int x2 = (int)AET[i].x;
+                if (x1 > x2)
+                    Common.Swap<int>(ref x1, ref x2);
+                for(int width = x1; width <= x2; width++)
+                {
+                    RenderBuffer.Instance.SetPixel(width, y);
+                }
+            }
+        }
+
+        void RemoveNonActiveEdgeFromAet(ref List<Edge> AET, int y)
+        {
+            for (int i = 0; i < AET.Count; i++)
+            {
+                if (AET[i].ymax == y)
+                {
+                    AET.RemoveAt(i);
+                }
+            }
+        }
+
+        void UpdateAndResortAet(ref List<Edge> AET)
+        {
+            for (int i = 0; i < AET.Count; i++)
+            {
+                AET[i].x += AET[i].dx;
+            }
+
+
+            AET.Sort(delegate (Edge a, Edge b)
+            {
+                if (a.x <= b.x)
+                    return 1;
+                else
+                    return -1;
+            });
+        }
+
+        void HorizonEdgeFill()
+        {
+            for(int i=0; i < Vertices.Count; i++)
+            {
+                Vector2d StartPoint = Vertices[i];
+                Vector2d EndPoint = Vertices[(i + 1) % Vertices.Count];
+                if (StartPoint.y == EndPoint.y)
+                    Draw.Draw2DSegement(StartPoint, EndPoint);
+            }
+        }
+
+        void ProcessScanLineFill()
+        {
+            List<Edge> AET = new List<Edge>();
+            for (int y = ymin; y <= ymax; y++)
+            {
+                InsertNetListToAet(EdgeTable[y - ymin],ref AET);
+                FillAetScanLine(AET, y);
+                RemoveNonActiveEdgeFromAet(ref AET, y);
+                UpdateAndResortAet(ref AET);
+            }
+        }
+
+    }
+
+}
